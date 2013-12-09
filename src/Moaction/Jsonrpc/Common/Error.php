50d44a69a4c983c8813bbd8c5d3bb8cde0f5ca94
Alexey Kalinin <alexey.kalinin@moaction.ru>
2013-12-09 14:54:00 +0400

    Error message strings

--- src/Moaction/Jsonrpc/Common/Error.php
+++ src/Moaction/Jsonrpc/Common/Error.php
@@ -4,11 +4,30 @@ namespace Moaction\Jsonrpc\Common;
 
 class Error
 {
-	const ERROR_PARSE_ERROR = -32700;
-	const ERROR_INVALID_REQUEST = -32600;
+	/**
+	 * Protocol error codes
+	 */
+	const ERROR_PARSE_ERROR      = -32700;
+	const ERROR_INTERNAL_ERROR   = -32603;
+	const ERROR_INVALID_PARAMS   = -32602;
 	const ERROR_METHOD_NOT_FOUND = -32601;
-	const ERROR_INVALID_PARAMS = -32602;
-	const ERROR_INTERNAL_ERROR = -32603;
+	const ERROR_INVALID_REQUEST  = -32600;
+	const ERROR_SERVER_ERROR     = -32000;
+
+	/**
+	 * Protocol error messages
+	 *
+	 * @var array
+	 */
+	private $errorMessages = array(
+		self::ERROR_INTERNAL_ERROR   => 'Internal error',
+		self::ERROR_INVALID_PARAMS   => 'Invalid params',
+		self::ERROR_INVALID_REQUEST  => 'Invalid Request',
+		self::ERROR_METHOD_NOT_FOUND => 'Method not found',
+		self::ERROR_PARSE_ERROR      => 'Parse error',
+		self::ERROR_SERVER_ERROR     => 'Server error',
+	);
+
 	/**
 	 * @var int
 	 */
@@ -27,7 +46,7 @@ class Error
 	/**
 	 * @param int $code
 	 * @param string $message
-	 * @param null $data
+	 * @param mixed $data
 	 */
 	public function __construct($code = null, $message = null, $data = null)
 	{
@@ -37,11 +56,14 @@ class Error
 	}
 
 	/**
+	 * Get error code
+	 *
+	 * @param int $defaultCode default return code if not set
 	 * @return int
 	 */
-	public function getCode()
+	public function getCode($defaultCode = self::ERROR_SERVER_ERROR)
 	{
-		return $this->code;
+		return $this->code ?: $defaultCode;
 	}
 
 	/**
@@ -53,19 +75,61 @@ class Error
 	}
 
 	/**
+	 * Get error message
+	 *
 	 * @return string
 	 */
 	public function getMessage()
 	{
+		if (!$this->message) {
+			return $this->getMessageString($this->getCode());
+		}
+
 		return $this->message;
 	}
 
+	/**
+	 * Create object from array
+	 *
+	 * @param array $data
+	 * @return self
+	 */
 	public static function fromArray(array $data)
 	{
 		return new self(
-			!empty($data['code']) ? (int)$data['code'] : null,
-			!empty($data['message']) ? $data['message'] : null,
-			!empty($data['data']) ? $data['data'] : null
+			!empty($data['code'])    ? (int)$data['code'] : null,
+			!empty($data['message']) ? $data['message']   : null,
+			!empty($data['data'])    ? $data['data']      : null
 		);
+	}
+
+	/**
+	 * Transform object to array
+	 *
+	 * @return array
+	 */
+	public function toArray()
+	{
+		$error = array(
+			'code'    => $this->getCode(),
+			'message' => $this->getMessage(),
+		);
+
+		if ($this->getData()) {
+			$error['data'] = $this->getData();
+		}
+
+		return $error;
+	}
+
+	/**
+	 * Return message string by code
+	 *
+	 * @param $errorCode
+	 * @return string|null
+	 */
+	public function getMessageString($errorCode)
+	{
+		return isset($this->errorMessages[$errorCode]) ? $this->errorMessages[$errorCode] : null;
 	}
 } 
